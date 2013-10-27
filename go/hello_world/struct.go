788c1eb0b09c44465a464ceabb533f5d2f2cb10f
MathYourLife <daniel.robert.couture@gmail.com>
2013-10-26 23:30:49 -0400

    Go basics

---
+++ go/hello_world/struct.go
@@ -0,0 +1,68 @@
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+    X, Y int
+}
+
+var (
+    p = Vertex{1, 2}  // has type Vertex
+    q = &Vertex{1, 2} // has type *Vertex
+    r = Vertex{X: 1}  // Y:0 is implicit
+    s = Vertex{}      // X:0 and Y:0
+)
+
+func main() {
+    fmt.Println(p, q, r, s)
+
+	slice()
+	slice2()
+	array()
+}
+
+func slice() {
+    p := []int{2, 3, 5, 7, 11, 13}
+    fmt.Println("p ==", p)
+
+    for i := 0; i < len(p); i++ {
+        fmt.Printf("p[%d] == %d\n",
+            i, p[i])
+    }
+}
+
+
+
+func slice2() {
+    a := make([]int, 5)
+    printSlice("a", a)
+    b := make([]int, 0, 5)
+    printSlice("b", b)
+    c := b[:2]
+    printSlice("c", c)
+    d := c[2:5]
+    printSlice("d", d)
+}
+
+func printSlice(s string, x []int) {
+    fmt.Printf("%s len=%d cap=%d %v\n",
+        s, len(x), cap(x), x)
+}
+
+
+
+
+type GPS struct {
+    Lat, Long float64
+}
+
+var m map[string]GPS
+
+func array () {
+    m = make(map[string]GPS)
+    m["Bell Labs"] = GPS{
+        40.68433, -74.39967,
+    }
+    fmt.Println(m["Bell Labs"])
+}
